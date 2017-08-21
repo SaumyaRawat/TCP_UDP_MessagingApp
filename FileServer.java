10bf8658e4744b40deffad92b522bb8c49caeaf3
Saumya Rawat <saumya.rawat25@gmail.com>
2017-08-21 15:36:19 +0530

    Added file transfer

---
+++ FileServer.java
@@ -0,0 +1,90 @@
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.DataInputStream;
+import java.io.FileOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class FileServer extends Thread {
+	
+	private ServerSocket ss;
+	
+	public FileServer(int port) {
+		try {
+			ss = new ServerSocket(port);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void run() {
+		while (true) {
+			try {
+				Socket clientSock = ss.accept();
+				saveFile(clientSock);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void saveFile(Socket clientSock) throws IOException {
+		DataInputStream dis = new DataInputStream(clientSock.getInputStream());
+		FileOutputStream fos = new FileOutputStream("testfile.java");
+		byte[] buffer = new byte[4096];
+		
+		int filesize = 15123; // Send file size in separate msg
+		int read = 0;
+		int totalRead = 0;
+		int remaining = filesize;
+		while((read = dis.read(buffer, 0, Math.min(buffer.length, remaining))) > 0) {
+			totalRead += read;
+			remaining -= read;
+			System.out.println("read " + totalRead + " bytes.");
+			fos.write(buffer, 0, read);
+		}
+		
+		fos.close();
+		dis.close();
+	}
+	
+	public static void main(String[] args) {
+		FileServer fs = new FileServer(1988);
+		fs.start();
+	}
+
+}
+class FileClient {
+	
+	private Socket s;
+	
+	public FileClient(String host, int port, String file) {
+		try {
+			s = new Socket(host, port);
+			sendFile(file);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+	}
+	
+	public void sendFile(String file) throws IOException {
+		DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+		FileInputStream fis = new FileInputStream(file);
+		byte[] buffer = new byte[4096];
+		
+		while (fis.read(buffer) > 0) {
+			dos.write(buffer);
+		}
+		
+		fis.close();
+		dos.close();	
+	}
+	
+	public static void main(String[] args) {
+		FileClient fc = new FileClient("localhost", 1988, "FileServer.java");
+	}
+
+}
+
+
